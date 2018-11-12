d8a8ccceb3a21a0a41c516eb0f3ac6b2424b54c6
tv.unimol <doslovno@gmail.com>
2018-11-12 21:06:59 +0300

    show crimeFragment with Crime when you click on any crime in CrimeListFragment

--- app/src/main/java/com/example/manifest/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/manifest/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.example.manifest.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -64,9 +65,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(getActivity(),
-                    crime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
-                    .show();
+            Intent intent = CrimeActivity.newIntent(getActivity(), crime.getId());
+            startActivity(intent);
         }
     }
 
