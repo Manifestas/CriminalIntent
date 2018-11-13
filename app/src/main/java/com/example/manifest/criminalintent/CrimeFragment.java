78161928389b006f3facf3fe8e4fa19fb5453642
tv.unimol <doslovno@gmail.com>
2018-11-13 19:36:46 +0300

    add DialogFragment when user click on DateButton

--- app/src/main/java/com/example/manifest/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/manifest/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -19,6 +20,8 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
+
     private Crime crime;
     private EditText titleField;
     private Button dateButton;
@@ -66,7 +69,11 @@ public class CrimeFragment extends Fragment {
 
         dateButton = (Button) v.findViewById(R.id.crime_date);
         dateButton.setText(crime.getFormattedDate());
-        dateButton.setEnabled(false);
+        dateButton.setOnClickListener(view -> {
+            FragmentManager fragmentManager = getFragmentManager();
+            DatePickerFragment dialog = new DatePickerFragment();
+            dialog.show(fragmentManager, DIALOG_DATE);
+        });
 
         solvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
         solvedCheckBox.setChecked(crime.isSolved());
